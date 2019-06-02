7de8fb98de5e7c1fa823ad97be293347f15e3648
EvaLourenco <37893245+EvaLourenco@users.noreply.github.com>
2019-06-02 20:36:49 -0300

    Add files via upload

---
+++ Fatec-atividade4-eva/public_html/js/script.js
@@ -0,0 +1,121 @@
+  $( function() {
+    $( "#accordion" ).accordion({
+      collapsible: true,
+      active: false      
+    });
+  } );
+
+//Função para o recurso de collapse -- Não utilizei
+ function ativar(btn) {
+    var btnID = btn.id;
+    var divID = btnID.substr(3);
+    
+    var div = document.getElementById(divID);
+    
+    if (div.style.display == 'none') {
+       div.style.display = 'block' ;
+    } else
+        div.style.display = 'none';     
+  
+    
+}
+
+
+
+
+function validar(){
+    var duvidas = $("#duvidas").val();
+    var nome = $ ("#nome").val();
+    var email= $ ("#email").val();
+    var erro = false;
+    //Valida Nome
+if (nome.length <=9) {
+    $(document).ready(function(){
+        $.notify({
+         message:"ERRO! Campo Nome deve ter no mínimo 10 caracteres"
+    },{
+        type: 'danger',
+        timer: 1000
+});
+});
+
+erro =true;
+}
+
+//Verifica espaço após nome
+if (nome.trim().indexOf(" ") === -1) {
+    $.notify({
+        message:"Campo nome deve ter um sobrenome!"
+    },{
+            type:('danger'),
+            timer:1000
+});
+    erro = true;
+}
+
+//Valida e-mail
+var sEmail	= $("#email").val();
+		var emailFilter=/^.+@.+\..{2,}$/;
+		var illegalChars= /[\(\)\<\>\,\;\:\\\/\"\[\]]/;		
+                if(!(emailFilter.test(sEmail))||sEmail.match(illegalChars)){
+		$(document).ready(function(){
+        $.notify({
+            message:"E-mail inválido!"
+    },{
+        type: 'danger',
+        timer: 1000
+});
+});
+erro =true;
+}	
+
+//Verifica espaços no e-mail
+if ( email.search( /\s/g ) !== -1 ){
+		$.notify({
+                    message:"Campo e-mail com espaços em branco"
+                },{
+                    type:'danger',
+                    timer:1000
+            });
+            email = email.replace( /\s/g , "" );
+		
+erro =true;
+}
+
+//Valida dúvidas
+if (duvidas.length > 400 ) {
+$(document).ready(function(){
+    $.notify({
+        message:"Campo duvidas excede 400 caracteres"
+    },{
+        type: 'danger',
+        timer: 1000
+});
+});
+erro =true;
+}
+if (duvidas.length < 1 ) {
+$(document).ready(function(){
+    $.notify({
+        message:"Campo duvidas está em branco"
+    },{
+        type: 'danger',
+        timer: 2000
+    
+});
+});
+erro =true;
+}
+
+//Envia fomulário e gera msg
+if (erro === false) {
+    $.notify({
+        message:"Dúvida enviada com sucesso!"
+   },{
+        type: 'success',
+        timer:2000
+  });
+
+}
+return false;
+}
